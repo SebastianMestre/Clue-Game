a3e0c56282afb11632db557df66018893ef167a8
SebastianMestre <mestresebastianunr@gmail.com>
2018-04-08 19:50:35 -0300

    added main.c

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define ever (;;)
+
+const int MIN_PLAYER_COUNT = 3;
+const int MIN_PLAYER_COUNT = 6;
+int playerCount = 0;
+
+int main(){
+
+  for ever {
+    scanf("%d\n", &playerCount);
+    if (playerCount < MIN_PLAYER_COUNT) {
+      printf("Se requieren al menos %d jugadores", MIN_PLAYER_COUNT);
+    } else if (playerCount > MAX_PLAYER_COUNT) {
+      printf("Se permiten a lo sumo %d jugadores", MAX_PLAYER_COUNT);
+    } else {
+      break;
+    }
+  }
+
+  printf("%d\n", playerCount);
+
+  return 0;
+}
