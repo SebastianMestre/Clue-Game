6aa87fdf85d1b77ff6ca0351157f843e392df037
SebastianMestre <mestresebastianunr@gmail.com>
2018-04-10 20:07:20 -0300

    repara una pila de errores

--- src/main.c
+++ src/main.c
@@ -1,14 +1,21 @@
 #include <stdbool.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
-#define swap(a,b,T) do{T temp = a; a = b; b = temp}while(0)
+#define swap(a,b,T) do{T temp = a; a = b; b = temp;}while(0)
 
-const int MIN_PLAYER_COUNT = 3;
-const int MAX_PLAYER_COUNT = 6;
-const int SUSPECT_COUNT = 6;
-const int WEAPON_COUNT = 6;
-const int PLACE_COUNT = 9;
+// const int MIN_PLAYER_COUNT = 3;
+// const int MAX_PLAYER_COUNT = 6;
+// const int SUSPECT_COUNT = 6;
+// const int WEAPON_COUNT = 6;
+// const int PLACE_COUNT = 9;
+
+#define MIN_PLAYER_COUNT 3
+#define MAX_PLAYER_COUNT 6
+#define SUSPECT_COUNT 6
+#define WEAPON_COUNT 6
+#define PLACE_COUNT 9
 
 struct jugador{
   int location;
@@ -24,19 +31,19 @@ typedef struct jugador player_t;
 player_t players[MAX_PLAYER_COUNT];
 int playerCount = 0;
 
-const char PLACE_NAMES [30][] = {
+const char PLACE_NAMES [][30] = {
   "Cocina", "Patio", "Spa",
   "Teatro", "Salon", "Observatorio",
   "Vestibulo", "Habitacion", "Comedor"
 };
 
-const char SUSPECT_NAMES [30][] = {
+const char SUSPECT_NAMES [][30] = {
   "Senorita Escarlata", "Coronel Mostaza",
   "Profesor Moradillo", "Senora Azulino",
   "Senor Verdi", "Senora Blanco"
 };
 
-const char WEAPON_NAMES [30][] = {
+const char WEAPON_NAMES [][30] = {
   "revolver", "cuchillo", "lazo",
   "candelabro", "llave inglesa", "tubo de plomo"
 };
@@ -76,7 +83,7 @@ int main(){
   shuffleIntList(randlist, SUSPECT_COUNT);
 
   for(int i = 0; i < SUSPECT_COUNT; i++){
-    players[i % playerCount].suspects[i / playerCount] = ;
+    players[i % playerCount].suspects[i / playerCount] = 0;
   }
 
   free(randlist);
