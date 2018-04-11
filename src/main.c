62d66e54a74bc3750ded9e0f9edf7b6ff560c9dc
SebastianMestre <mestresebastianunr@gmail.com>
2018-04-11 18:56:26 -0300

    crea el header y el archivo de implementacion random_list

--- src/main.c
+++ src/main.c
@@ -48,22 +48,6 @@ const char WEAPON_NAMES [][30] = {
   "candelabro", "llave inglesa", "tubo de plomo"
 };
 
-int* makeIntList(int start, int end){
-  int len = end - start;
-  int* res = malloc(len * sizeof(int));
-  for(int i = 0; i < len; i++){
-    res[i] = i + start;
-  }
-  return res;
-}
-
-void shuffleIntList(int* p_list, int len){
-  for(int i = 1; i < len; i++){
-    int ran = rand() % i;
-    swap(p_list[i], p_list[ran], int);
-  }
-}
-
 int main(){
   srand(time(NULL));
 
---
+++ src/random_list.h
@@ -0,0 +1,10 @@
+#ifndef RANDOM_LIST_H
+#define RANDOM_LIST_H
+// random_list.h
+
+int* makeIntList(int start, int end);
+
+void shuffleIntList(int* p_list, int len);
+
+
+#endif /* end of include guard: RANDOM_LIST_H */
