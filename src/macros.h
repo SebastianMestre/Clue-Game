b07371104fdc269ff75fc28fde0beac068f49ac6
Emmanuel M <emmanuelbendavid@gmail.com>
2018-04-11 23:01:59 -0300

    Used strtoul() to check for correct input after scanf() obtains char[1]

--- src/macros.h
+++ src/macros.h
@@ -3,5 +3,6 @@
 // macros.h
 
 #define swap(a,b,T) do{T temp = a; a = b; b = temp;}while(0)
+#define purge_input_buffer() char c; while ((c = getchar()) != '\n' && c != EOF);
 
 #endif /* end of include guard: CLUE_MACROS_H */
--- src/main.c
+++ src/main.c
@@ -15,11 +15,16 @@ player_t players[MAX_PLAYER_COUNT];
 int main(){
   srand(time(NULL));
 
-  int playerCount = 0;
+  unsigned int playerCount;
 
   while (true) {
     puts("Ingrese la cantidad de jugadores :");
-    scanf("%d\n", &playerCount);
+
+    char input[5];
+    scanf("%5s", input);
+    playerCount = (unsigned int) strtoul(input, NULL, 10);
+    purge_input_buffer()
+
     if (playerCount < MIN_PLAYER_COUNT) {
       printf("Se requieren al menos %d jugadores", MIN_PLAYER_COUNT);
     } else if (playerCount > MAX_PLAYER_COUNT) {
