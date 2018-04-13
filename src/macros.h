cb404309456a72fcdf35f03b904a11aa289d1f09
SebastianMestre <mestresebastianunr@gmail.com>
2018-04-13 08:40:53 -0300

    Se agregan parentesis a los macros

--- src/macros.h
+++ src/macros.h
@@ -4,10 +4,10 @@
 
 #define CAT(a,b) a##b
 
-#define swap(a,b,T) do{T temp = a; a = b; b = temp;}while(0)
+#define swap(a,b,T) do{T temp = (a); (a) = (b); (b) = temp;}while(0)
 
-#define cp(source, dest, size) for(int i=0; i<size; i++) dest[i] = source[i]
+#define cp(source, dest, size) for(int i=0; i<(size); i++) (dest)[i] = (source)[i]
 
-#define isdigit(x) (x>-1 && x<10)
+#define isdigit(x) ((x)>-1 && (x)<10)
 
 #endif /* end of include guard: CLUE_MACROS_H */
