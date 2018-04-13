a9b07d47d9ce3609d2851ceb5994c067cd9925d3
Gastonm123 <38297466+Gastonm123@users.noreply.github.com>
2018-04-13 00:39:01 -0300

    Update movement.c

--- src/movement.c
+++ src/movement.c
@@ -1,8 +1,9 @@
+#include <stdbool.h>
+#include <stdio.h>
+
 #include "constants.h"
 #include "macros.h"
 #include "player.h"
-#include <stdbool.h>
-#include <stdio.h>
 
 #define forMov(mode, expression) \
 for(int i = 0; i < tiradaDado; i++){ \
@@ -12,7 +13,6 @@ for(int i = 0; i < tiradaDado; i++){ \
     cpHabitaciones[aux] = 1; \
     habitacionesAnunciadas[it++] = aux; \
 }}
-#define isdigit(n) (n > -1 && n < 10)
 
 
 int mapCorners(int corner){
