5f330862bf12b65277555cb699650b896b7a32c0
SebastianMestre <mestresebastianunr@gmail.com>
2018-04-12 23:25:39 -0300

    incluye player.h desde player.c, acorta codigo en implementacion de deal_player usando un macro

--- src/player.c
+++ src/player.c
@@ -1,37 +1,26 @@
+#include "player.h"
 // player.c
+
+#define PLAYER_SHUFFLE_DEAL(lowcase,upcase) do{                                 \
+    int* randlist = makeIntList(0, CAT(upcase,_COUNT));                         \
+    shuffleIntList(randlist, CAT(upcase,_COUNT));                               \
+    for(int i = 0; i < CAT(upcase, _COUNT); i++; player_idx++){                 \
+      players[player_idx % playerCount].lowcase[i / playerCount] = randlist[i]; \
+      players[player_idx % playerCount].lowcase[i / playerCount + 1] = -1;      \
+    }                                                                           \
+    free(randlist);                                                             \
+  }while(0)
 
 void player_deal(size_t playerCount){
 
   // player index (mod playerCount)
   int player_idx = 0;
-  int* randlist;
 
-  randlist = makeIntList(0, SUSPECT_COUNT);
-  shuffleIntList(randlist, SUSPECT_COUNT);
+  PLAYER_SHUFFLE_DEAL(suspects,SUSPECT);
 
-  for(int i = 0; i < SUSPECT_COUNT; i++; player_idx++){
-    players[player_idx % playerCount].suspects[i / playerCount] = randlist[i];
-    players[player_idx % playerCount].suspects[i / playerCount + 1] = -1;
-  }
+  PLAYER_SHUFFLE_DEAL(weapons,WEAPON);
 
-  free(randlist);
-
-  randlist = makeIntList(0, WEAPON_COUNT);
-  shuffleIntList(randlist, WEAPON_COUNT);
-
-  for(int i = 0; i < WEAPON_COUNT; i++; player_idx++){
-    players[player_idx % playerCount].weapons[i / playerCount] = randlist[i];
-    players[player_idx % playerCount].weapons[i / playerCount + 1] = -1;
-  }
-
-  free(randlist);
-
-  randlist = makeIntList(0, PLACE_COUNT);
-  shuffleIntList(randlist, PLACE_COUNT);
-
-  for(int i = 0; i < PLACE_COUNT; i++; player_idx++){
-    players[player_idx % playerCount].places[i / playerCount] = randlist[i];
-    players[player_idx % playerCount].places[i / playerCount + 1] = -1;
-  }
-
+  PLAYER_SHUFFLE_DEAL(places,PLACE);
 }
+
+#undef PLAYER_SHUFFLE_DEAL
